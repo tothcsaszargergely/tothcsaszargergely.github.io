fde98cc410bc7c0a66c54183d401cfc6c76264d0
tothcsaszargergely <toth.gergely@gmail.com>
2019-02-23 15:05:23 +0100

    Change 10

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 console.log('Mukodik')
 
+fb.ref("postok").once('value').then(data => {
+data.forEach(element => {
+    $('.doboznagy').append(`
+    <div class="doboz">
+        <h1>${element.child('title').val()}</h1>
+        <h2>${element.child('time').val()}</h2>
+        <p>${element.child('content').val()}</p>
+    </div>
+    `);
+});
+});
+
 $('button').on('click', (event) => {
     event.preventDefault();
     let title = $('#title').val()
