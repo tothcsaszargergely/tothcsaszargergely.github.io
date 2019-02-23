53cf47ef0ee08b72f2909bce563f2470c1f03ec5
tothcsaszargergely <toth.gergely@gmail.com>
2019-02-23 16:52:18 +0100

    Change 14

--- index.js
+++ index.js
@@ -20,21 +20,22 @@ $('button').on('click', (event) => {
     console.log (time);
     let content = $('#content').val()
     console.log (content);
-    let dataToSave = { title: title, time: time, content: content};
-    fb.ref("postok").push(dataToSave);
+
 
     if (title === ""|| time === ""|| content === "") {     
-    }
+        }
     else {
-        $('.doboznagy').append(
-            `
-            <div class="doboz">
-            <h1>${title}</h1>
-            <h2>${time}</h2>
-            <p>${content}</p>
-            </div>
-            `
-        );
-        $('input').val("");  
-        }    
+            $('.doboznagy').append(
+                `
+                <div class="doboz">
+                <h1>${title}</h1>
+                <h2>${time}</h2>
+                <p>${content}</p>
+                </div>
+                `
+            );
+            let dataToSave = { title: title, time: time, content: content};
+            fb.ref("postok").push(dataToSave);
+            $('input').val("");  
+            }    
     });
